82936456b8f4d746276ec981562c641587b98ef8
mayc01 <yanchaoma@foxmail.com>
2021-05-21 13:34:37 +0800

    提取图结构并以图搜图将结果转换为虚拟图

--- src/test/java/data/lab/ongdb/shortestPath/ShortestPathTest.java
+++ src/test/java/data/lab/ongdb/shortestPath/ShortestPathTest.java
@@ -4,7 +4,6 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.neo4j.graphdb.GraphDatabaseService;
 import org.neo4j.graphdb.Result;
-import org.neo4j.graphdb.Transaction;
 import org.neo4j.harness.junit.Neo4jRule;
 
 import java.util.ArrayList;
@@ -25,43 +24,41 @@ import java.util.Map;
  */
 public class ShortestPathTest {
     @Rule
-    public Neo4jRule neo4j = new Neo4jRule()
-            .withProcedure(ShortestPath.class);
+    public Neo4jRule neo4j = new Neo4jRule().withProcedure(ShortestPath.class);
 
     @Test
     public void allPathsTightness() {
         GraphDatabaseService db = neo4j.getGraphDatabaseService();
 
-        try (Transaction tx = db.beginTx()) {
+        List<Long> sourceIds = new ArrayList<>();
+        sourceIds.add(123l);
+        sourceIds.add(223l);
+        sourceIds.add(223l);
 
-            List<Long> sourceIds = new ArrayList<>();
-            sourceIds.add(123l);
-            sourceIds.add(223l);
-            sourceIds.add(223l);
+        List<Long> targetIds = new ArrayList<>();
+        targetIds.add(32423l);
+        targetIds.add(4353l);
+        targetIds.add(4353l);
 
-            List<Long> targetIds = new ArrayList<>();
-            targetIds.add(32423l);
-            targetIds.add(4353l);
-            targetIds.add(4353l);
+        List<Double> distanceSTE = new ArrayList<>();
+        distanceSTE.add(23.0);
+        distanceSTE.add(324.0);
+        distanceSTE.add(324.0);
 
-            List<Double> distanceSTE = new ArrayList<>();
-            distanceSTE.add(23.0);
-            distanceSTE.add(324.0);
-            distanceSTE.add(324.0);
+        Map<String, Object> map = new HashMap<>();
+        map.put("sourceIds", sourceIds);
+        map.put("targetIds", targetIds);
+        map.put("distanceSTE", distanceSTE);
 
-            Map<String, Object> map = new HashMap<>();
-            map.put("sourceIds", sourceIds);
-            map.put("targetIds", targetIds);
-            map.put("distanceSTE", distanceSTE);
+        Result res = db.execute("CALL olab.shortestPath.allPathsTightness({sourceIds},{targetIds},{distanceSTE}) YIELD source,target,distance,tightnessSort RETURN source,target,distance,tightnessSort", map);
 
-            Result res = db.execute("CALL olab.shortestPath.allPathsTightness({sourceIds},{targetIds},{distanceSTE}) YIELD source,target,distance,tightnessSort RETURN source,target,distance,tightnessSort", map);
-
-            while (res.hasNext()) {
-                Map map2 = res.next();
-                System.out.println(map2.get("source") + " " + map2.get("target") + " " + map2.get("distance") + " " + map2.get("tightnessSort"));
-            }
-
+        while (res.hasNext()) {
+            Map map2 = res.next();
+            System.out.println(map2.get("source") + " " + map2.get("target") + " " + map2.get("distance") + " " + map2.get("tightnessSort"));
         }
+
     }
 
 }
+
+
